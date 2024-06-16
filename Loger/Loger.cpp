5d7176bbb813fb5cc6b38e2a7e3eb475cda3b7e7
Avenazh <najik1010@mail.ru>
2024-06-16 05:20:35 +0300

    Мы старались)

--- Loger/Loger.cpp
+++ Loger/Loger.cpp
@@ -4,11 +4,16 @@
 int main()
 {
 	LOG_FILE_DEFAULT();
+
 	Logger::ifError(__FILE__, __LINE__, "%s----%d%d", "nenennee", 123, 1111);
+
 	LOG_FILE_CUSTOM("NEW");
+
 	LOG_TRACE("nenennee");
 	LOG_DEBUG("debagigigigi");
+
 	LOG_CLOSE_CUSTOM("NEW");
+
 	LOG_DEBUG("deb23235235235235235235i");
 	LOG_TRACE("123");
 
